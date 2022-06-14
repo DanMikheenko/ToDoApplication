d63c9ef6413857f931ce033b7c4b0df5129683ee
Dan Mikheenko <miheenko.denis@gmail.com>
2022-06-14 17:49:48 +0300

    Add project files.

---
+++ ToDo/DbRepository/DbOperations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ToDo.Models;
+
+namespace ToDo.DbRepository
+{
+    internal class DbOperations
+    {
+        private readonly string path;
+
+        public DbOperations(string path)
+        {
+            this.path = path;
+        }
+
+        public BindingList<ToDoModel>? Load()
+        {
+            var isFileExists = File.Exists(path);
+            if (!isFileExists)
+            {
+                File.CreateText(path).Dispose();
+                return new BindingList<ToDoModel>();
+            }
+
+            using (var reader = File.OpenText(path))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<ToDoModel>>(fileText);
+            }
+        }
+
+        public void Save(BindingList<ToDoModel> toDoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(path))
+            {
+                string output = JsonConvert.SerializeObject(toDoDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
---
+++ ToDo/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDo.DbRepository;
+using ToDo.Models;
+
+namespace ToDo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<ToDoModel> _toDoData;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var dbOperations = new DbOperations("///data.json");
+
+            try
+            {
+                _toDoData = dbOperations.Load();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                this.Close();
+            }
+            
+            _toDoData.ListChanged += ToDoDataOnListChanged;
+        }
+
+        private void ToDoDataOnListChanged(object? sender, ListChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgToDoList.ItemsSource = _toDoData;
+        }
+    }
+}
